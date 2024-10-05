863e364354009d22db6e86a9d364f52c09d3ec8a
Peeyush <69499613+PeeyushSachan@users.noreply.github.com>
2024-10-05 10:56:32 +0530

    sarted

---
+++ lib/provider/myprovider.dart
@@ -0,0 +1,14 @@
+class Myprovider extends StatefulWidget {
+
+  @override
+  _MyproviderState createState() => _MyproviderState();
+}
+
+class _MyproviderState extends State<Myprovider> {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      
+    );
+  }
+}
